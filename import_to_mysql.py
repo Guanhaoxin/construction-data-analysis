cbb616417971ce2fd7d6051c9101a5d3d168fdff
关皓昕 <168891951@qq.com>
2026-04-21 18:08:45 +0800

    Add password input for MySQL connection
    Prompt user for MySQL root password before importing data.
    fix: remove hardcoded password

--- import_to_mysql.py
+++ import_to_mysql.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+password = input("请输入 MySQL root 密码: ")
 df = pd.read_csv('projects.csv')
-engine = create_engine('mysql+pymysql://root:Ghx2005101.@localhost/construction_db')
+engine = create_engine(f'mysql+pymysql://root:{password}@localhost/construction_db')
 df.to_sql('projects', engine, if_exists='replace', index=False)
-print("导入成功，共", len(df), "条")
+print("导入成功，共", len(df), "条记录")
